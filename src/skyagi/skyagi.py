ef00cca4ae810597a0740450397dbdd265272312
Tan Li <tan@tanli.dev>
2023-04-17 04:33:09 -0700

    update

--- src/skyagi/skyagi.py
+++ src/skyagi/skyagi.py
@@ -2,17 +2,21 @@ class Memory:
     def __init__(self):
         pass
 
+
 class Record:
     def __init__(self):
         pass
+
 
 class Agent:
     def __init__(self, prompt: str):
         self.prompt = prompt
 
+
 class Recency:
     def __init__(self):
         pass
+
 
 class Importance:
     def __init__(self, prompt: str):
@@ -21,14 +25,21 @@ class Importance:
     def get_importance(self):
         return 0
 
+
 class Relevance:
     def __init__(self):
         pass
+
 
 class Reflection:
     def __init__(self):
         pass
 
+
 class Plan:
     def __init__(self):
         pass
+
+
+def retrieve_records(memory, context):
+    return []
