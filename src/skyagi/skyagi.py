3d061344cdf166059c1aa3545b9c6e6ed8476621
Tan Li <tan@tanli.dev>
2023-04-17 02:13:27 -0700

    update

--- src/skyagi/skyagi.py
+++ src/skyagi/skyagi.py
@@ -2,6 +2,10 @@ class Memory:
     def __init__(self):
         pass
 
+class Event:
+    def __init__(self, prompt: str):
+        self.prompt = prompt
+
 class Agent:
     def __init__(self, prompt: str):
         self.prompt = prompt
@@ -13,6 +17,9 @@ class Recency:
 class Importance:
     def __init__(self, prompt: str):
         self.prompt = prompt
+
+    def get_importance(self, event: Event):
+        return 0
 
 class Relevance:
     def __init__(self):
