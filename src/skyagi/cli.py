9af1e917b17f5baf423a911c393e598ce0abaf6f
Tan Li <tan@tanli.dev>
2023-04-16 18:41:10 -0700

    update

--- src/skyagi/cli.py
+++ src/skyagi/cli.py
@@ -1,8 +1,42 @@
 import typer
 from rich.console import Console
 
+from skyagi import config, tui
+
 cli = typer.Typer()
 console = Console()
+
+#######################################################################################
+# Config CLI
+
+config_cli = typer.Typer()
+
+@config_cli.callback(invoke_without_command=True)
+def config_main(ctx: typer.Context):
+    """
+    Configure SkyAGI
+    """
+    # only run without a command specified
+    if ctx.invoked_subcommand is not None:
+        return
+
+    skyagi_config = config.load_config()
+
+    console.print("SkyAGI's Configuration")
+
+    if (skyagi_config.get("openai", None)):
+        console.print("OpenAI Token is configured, good job!", style="green")
+    else:
+        console.print("OpenAI Token not configured yet! This is necessary to use SkyAGI", style="red")
+        console.print("To config OpenAI token: [yellow]skyagi config openai[/yellow]")
+
+    if (skyagi_config.get("pinecone", None)):
+        console.print("Pinecone Token is configured, good job!", style="green")
+    else:
+        console.print("Pinecone Token not configured yet! This is necessary to use SkyAGI", style="red")
+        console.print("To config Pinecone token: [yellow]skyagi config pinecone[/yellow]")
+
+cli.add_typer(config_cli, name="config")
 
 #######################################################################################
 # Main CLI
---
+++ src/skyagi/config.py
@@ -0,0 +1,4 @@
+import os
+
+def load_config():
+    return {}
