4b560cb830d876daadfb4affc554534ccbce10b0
Tan Li <tan@tanli.dev>
2023-04-17 02:10:52 -0700

    update

--- src/skyagi/skyagi.py
+++ src/skyagi/skyagi.py
@@ -2,13 +2,17 @@ class Memroy:
     def __init__(self):
         pass
 
+class Agent:
+    def __init__(self, prompt: str):
+        self.prompt = prompt
+
 class Recency:
     def __init__(self):
         pass
 
 class Importance:
-    def __init__(self):
-        pass
+    def __init__(self, prompt: str):
+        self.prompt = prompt
 
 class Relevance:
     def __init__(self):
