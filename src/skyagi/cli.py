8e0d773faf6148052128e8c9066e94712dbc6950
Tan Li <tan@tanli.dev>
2023-04-17 10:24:49 -0700

    [agent] setup creating agents (#8)
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- src/skyagi/cli.py
+++ src/skyagi/cli.py
@@ -1,8 +1,10 @@
 import typer
+import time
 from rich.console import Console
-from rich.prompt import Prompt, Confirm, IntPrompt
+from rich.prompt import Prompt, IntPrompt
 
-from skyagi import config, tui, util
+from skyagi import config, util
+from skyagi.skyagi import Agent, Context, step
 
 cli = typer.Typer()
 console = Console()
@@ -77,6 +79,35 @@ def status():
     Status of the SkyAGI
     """
     console.print("SkyAGI status")
+
+
+@cli.command("run")
+def run():
+    """
+    Run SkyAGI
+    """
+    agent_count = IntPrompt.ask("Number of characters to create?", default=3)
+    agents = []
+    for idx in range(agent_count):
+        console.print(f"Creating character {idx+1}")
+        name = Prompt.ask("What is the character's name?").strip()
+        personality = Prompt.ask("Please use 3~5 words describe the character's personality, [yellow]e.g. confident, creative...[/yellow]")
+        intro = Prompt.ask(f"A brief intro, [yellow]e.g. {name} is a famous singer...[/yellow]")
+        agent = Agent(name, personality, intro, idx==0)
+        agents.append(agent)
+        console.print(f"Successfully created character {name}", style="green")
+        time.sleep(0.5)
+    console.print("SkyAGI starting...")
+    console.print(f"Now, you are going to behave as {agents[0].name}")
+    ctx = Context(console, config.load_openai_token())
+    instruction = ""
+    while True:
+        step(agents, ctx, instruction)
+        console.print("What's your action? Q for quit, Enter for continue", style="yellow")
+        instruction = Prompt.ask().strip()
+        if (instruction == "Q" or instruction == "q"):
+            console.print("Quitting SkyAGI...")
+            break
 
 
 @cli.callback(invoke_without_command=True)
--- src/skyagi/skyagi.py
+++ src/skyagi/skyagi.py
@@ -1,35 +1,77 @@
-class Agent:
-    def __init__(self, prompt: str):
-        self.prompt = prompt
-        self.memory = []
+import openai
+from typing import List
+from rich.console import Console
 
+class Context:
+    def __init__(self, console: Console, openai_key: str) -> None:
+        self.clock = 0
+        self.console = console
+        self.openai_key = openai_key
+
+
+class Agent:
+    def __init__(self, name: str, personality: str, intro: str, is_human: bool = False) -> None:
+        self.name = name
+        self.personality = personality
+        self.intro = intro
+        self.is_human = is_human
+        self.memory = []
+        # prompt related
+        prompt_instruct = f"""{name} has a personality of {personality} and here is an intro:
+{intro}
+
+At each time I ender the command "New_TIME_STEP" I want you to list the time of day and the action Yuan Yu is taking. If he is in a conversation with some please show that conversation.
+
+Examples:
+
+NEW_TIME_STEP
+06:00am {name} is asleep
+
+NEW_TIME_STEP
+
+It is currently 9:00 am
+"""
+        self.prompt = [
+            {"role": "system", "content": f"You are the AI behind a NPC character called {name}"},
+            {"role": "user", "content": prompt_instruct},
+        ]
+
+    def step(self, instruction: str, ctx: Context) -> None:
+        self.prompt.append({
+            "role": "user",
+            "content": "NEW_TIME_STEP"
+        })
+        openai.api_key = ctx.openai_key
+        resp = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=self.prompt,
+
+        )
+        message = resp["choices"][0]["message"]
+        self.prompt.append(message)
+        ctx.console.print(message["content"])
 
 class Record:
-    def __init__(self):
+    def __init__(self) -> None:
         pass
 
 
 class Oberservation(Record):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
 
 
 class Plan(Record):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
 
 
 class Reflection(Record):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
 
 
-def run_agi(num_iterations, agents):
-    timer = 0
-    for iter in range(num_iterations):
-        pass
-    pass
-
-
-if __name__ == "__main__":
-    run_agi(3, [])
+def step(agents: List[Agent], ctx: Context, instruction: str) -> None:
+    ctx.clock += 1
+    for agent in agents:
+        agent.step(instruction, ctx)
