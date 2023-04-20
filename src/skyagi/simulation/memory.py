b0eea5fa21f4bcb543b5fbd31946e7fa377b1833
Tan Li <tan@tanli.dev>
2023-04-20 04:41:28 -0700

    update

--- src/skyagi/simulation/memory.py
+++ src/skyagi/simulation/memory.py
@@ -1,17 +1,4 @@
-import re
-from datetime import datetime, timedelta
-from typing import List, Optional, Tuple
-
-from pydantic import BaseModel, Field
-
-from langchain import LLMChain
 from langchain.chat_models import ChatOpenAI
-from langchain.docstore import InMemoryDocstore
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.prompts import PromptTemplate
-from langchain.retrievers import TimeWeightedVectorStoreRetriever
-from langchain.schema import BaseLanguageModel, Document
-from langchain.vectorstores import FAISS
 
 USER_NAME = "Person A" # The name you want to use when interviewing the agent.
 LLM = ChatOpenAI(max_tokens=1500) # Can be any LLM you want.
