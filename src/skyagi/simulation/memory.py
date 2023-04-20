2589205a38effa2794659505f1d6ba1b464b01ac
Tan Li <tan@tanli.dev>
2023-04-20 04:42:45 -0700

    update

--- src/skyagi/simulation/memory.py
+++ src/skyagi/simulation/memory.py
@@ -1,4 +1,5 @@
 from langchain.chat_models import ChatOpenAI
+from skyagi.simulation.agent import GenerativeAgent
 
 USER_NAME = "Person A" # The name you want to use when interviewing the agent.
 LLM = ChatOpenAI(max_tokens=1500) # Can be any LLM you want.
