03271fb638b06b09a3b16ad5a810110d575f0b76
Tan Li <tan@tanli.dev>
2023-04-20 04:46:48 -0700

    update

--- src/skyagi/simulation/memory.py
+++ src/skyagi/simulation/memory.py
@@ -1,6 +1,57 @@
+import math
+import faiss
+
 from langchain.chat_models import ChatOpenAI
+from langchain.docstore import InMemoryDocstore
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.retrievers import TimeWeightedVectorStoreRetriever
+from langchain.vectorstores import FAISS
+
 from skyagi.simulation.agent import GenerativeAgent
 
-USER_NAME = "Person A" # The name you want to use when interviewing the agent.
-LLM = ChatOpenAI(max_tokens=1500) # Can be any LLM you want.
+USER_NAME = "Person A"  # The name you want to use when interviewing the agent.
+LLM = ChatOpenAI(max_tokens=1500)  # Can be any LLM you want.
 
+
+def relevance_score_fn(score: float) -> float:
+    """Return a similarity score on a scale [0, 1]."""
+    # This will differ depending on a few things:
+    # - the distance / similarity metric used by the VectorStore
+    # - the scale of your embeddings (OpenAI's are unit norm. Many others are not!)
+    # This function converts the euclidean norm of normalized embeddings
+    # (0 is most similar, sqrt(2) most dissimilar)
+    # to a similarity function (0 to 1)
+    return 1.0 - score / math.sqrt(2)
+
+
+def create_new_memory_retriever():
+    """Create a new vector store retriever unique to the agent."""
+    # Define your embedding model
+    embeddings_model = OpenAIEmbeddings()
+    # Initialize the vectorstore as empty
+    embedding_size = 1536
+    index = faiss.IndexFlatL2(embedding_size)
+    vectorstore = FAISS(
+        embeddings_model.embed_query,
+        index,
+        InMemoryDocstore({}),
+        {},
+        relevance_score_fn=relevance_score_fn,
+    )
+    return TimeWeightedVectorStoreRetriever(
+        vectorstore=vectorstore, other_score_keys=["importance"], k=15
+    )
+
+
+tommie = GenerativeAgent(
+    name="Tommie",
+    age=25,
+    traits="anxious, likes design",  # You can add more persistent traits here
+    status="looking for a job",  # When connected to a virtual world, we can have the characters update their status
+    memory_retriever=create_new_memory_retriever(),
+    llm=LLM,
+    daily_summaries=[
+        "Drove across state to move to a new town but doesn't have a job yet."
+    ],
+    reflection_threshold=8,  # we will give this a relatively low number to show how reflection works
+)
