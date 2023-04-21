279c86ddd8ec1e7efe4e553580150669587b9fb6
Tan Li <tan@tanli.dev>
2023-04-20 20:53:45 -0700

    update

--- src/skyagi/skyagi.py
+++ src/skyagi/skyagi.py
@@ -1,3 +1,4 @@
+import os
 import openai
 from typing import List
 from rich.console import Console
@@ -84,6 +85,7 @@ def agi_step(agents: List[Agent], ctx: Context, instruction: str) -> None:
 
 def agi_init(agent_configs: List[dict], console: Console, openai_key: str, user_idx: int = 0) -> Context:
     ctx = Context(console, openai_key)
+    os.environ['OPENAI_API_KEY'] = openai_key
     user_agent_name = agent_configs[user_idx]["name"]
     agents = []
     for agent_config in agent_configs:
