a2419ecea34d3fcedac04012e744812a209bab83
Tan Li <tan@tanli.dev>
2023-04-21 12:11:32 -0700

    update

---
+++ src/skyagi/discord/agent.py
@@ -0,0 +1,18 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.getenv('TOKEN'))
