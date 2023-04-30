428a2b96005656d53412ce1764ea311ef545a7f0
Tan Li <tan@tanli.dev>
2023-04-30 15:50:13 -0700

    update

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
+import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
