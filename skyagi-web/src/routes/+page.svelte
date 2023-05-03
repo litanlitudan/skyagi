3a42487a84753254c75acf5572a6707ff9934eea
serenaqiaoo <121361687+serenaqiaoo@users.noreply.github.com>
2023-05-03 13:19:29 -0700

    [supabase] setup authentication skeleton (#67)
    * [supabase] user authentication skeleton
    
    * format files
    
    * files format
    
    * remove unused import
    
    * import
    
    * remove unused comment
    
    ---------
    
    Co-authored-by: Serena Qiao <serenaqiao@Serenas-MacBook-Pro.local>
    Co-authored-by: Tan Li <tan@tanli.dev>

--- skyagi-web/src/routes/+page.svelte
+++ skyagi-web/src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- src/routes/+page.svelte -->
+<script lang="ts">
+	import { Auth } from '@supabase/auth-ui-svelte';
+	import { ThemeSupa } from '@supabase/auth-ui-shared';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<svelte:head>
+	<title>User Management</title>
+</svelte:head>
+
+<div class="row flex-center flex">
+	<div class="col-6 form-widget">
+		<Auth
+			supabaseClient={data.supabase}
+			view="magic_link"
+			redirectTo={`${data.url}/logging-in?redirect=/`}
+			showLinks={false}
+			appearance={{ theme: ThemeSupa, style: { input: 'color: #fff' } }}
+		/>
+	</div>
+</div>
