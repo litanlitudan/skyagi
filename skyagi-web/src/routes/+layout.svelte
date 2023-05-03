6685ce35f87a5da52cadc9294381599a69ce8fb5
Serena Qiao <serenaqiao@Serenas-MacBook-Pro.local>
2023-05-02 23:28:45 -0700

    [supabase] user authentication skeleton

--- skyagi-web/src/routes/+layout.svelte
+++ skyagi-web/src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+	import '../styles.css';
 	import { browser } from '$app/environment';
 	import { page } from '$app/stores';
 	import { webVitals } from '$lib/vitals';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+	import type { LayoutData } from './$types';
 
 	// Vercel Analytics
 	let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
@@ -12,8 +16,27 @@
 			analyticsId
 		});
 	}
+
+	// Auth
+	export let data: LayoutData;
+
+	$: ({ supabase, session } = data);
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((event, _session) => {
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
 </script>
 
-<div class="container">
+<svelte:head>
+	<title>User Management</title>
+</svelte:head>
+
+<div class="container" style="padding: 50px 0 100px 0">
 	<slot />
 </div>
--- skyagi-web/src/routes/+layout.ts
+++ skyagi-web/src/routes/+layout.ts
@@ -1,6 +1,25 @@
-import { inject } from '@vercel/analytics';
+// src/routes/+layout.ts
+import { invalidate } from '$app/navigation';
+import {
+  PUBLIC_SUPABASE_ANON_KEY,
+  PUBLIC_SUPABASE_URL
+} from '$env/static/public';
+import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
+import type { LayoutLoad } from './$types';
 
-export const load: LayoutLoad = async ({}) => {
-	inject();
-    return {};
+export const load: LayoutLoad = async ({ fetch, data, depends }) => {
+  depends('supabase:auth');
+
+  const supabase = createSupabaseLoadClient({
+    supabaseUrl: PUBLIC_SUPABASE_URL,
+    supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+    event: { fetch },
+    serverSession: data.session
+  });
+
+  const {
+    data: { session }
+  } = await supabase.auth.getSession();
+
+  return { supabase, session };
 };
