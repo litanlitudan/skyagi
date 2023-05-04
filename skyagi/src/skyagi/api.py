ee47c9c5c3192773c074137f010d9da034f8eafe
Qi Zheng <zhengqi1987@gmail.com>
2023-05-04 02:10:25 -0400

    have some skeleton but need to figure out how to wrap up the message

--- skyagi/src/skyagi/api.py
+++ skyagi/src/skyagi/api.py
@@ -9,7 +9,10 @@ from rich.prompt import IntPrompt, Prompt
 from skyagi import config, util
 from skyagi.discord import client
 from skyagi.skyagi import agi_init, agi_step
+
+import asyncio
 from lcserve import serving
+from typing import List
 
 cli = typer.Typer()
 console = Console()
@@ -124,34 +127,59 @@ def status():
     console.print("Discord client starting...", style="yellow")
     client.run(discord_token)
 
+async def send_ws_message(websocket, message: str):
+    if websocket is not None:
+        await websocket.send_json(
+            {'result': message, 'error': '', 'stdout': ''}
+        )
+    print(message)
+
+
+def report_error(websocket, err_msg: str):
+    asyncio.run(send_ws_message(websocket, err_msg))
+
+# TODO: wrapper for Prompt.ask
+def ask_human(websocket, message: str):
+    asyncio.run(send_ws_message(websocket, message))
+    return input()
 
 @serving(websocket=True)
-def runskyagi():
+def runskyagi(agent_configs: List[dict], **kwargs):
     """
     Run SkyAGI
     """
-    # TODO: info needed 
-    # any service token (like openai token)
-    # number of agents
-    # each agent's profile/config in json
+    websocket = kwargs.get('websocket')
+    openai_token = kwargs.get('openai_token') #TODO: make this more general
 
-    user_role = Prompt.ask(
-        "Pick which role you want to perform? (input the exact name, case sensitive)",
-        choices=agent_names,
-        default=agent_names[0],
-    )
+    if len(agent_configs) <= 2:
+        # TODO: return error
+        report_error(websocket, "Please config at least 2 agents, exiting")
+        return
+    
+    agent_names = []
+    for agent_config in agent_configs:
+        agent_names.append(agent_config["name"])
+
+    # ask for the user's role
+    user_role = ask_human("Pick which role you want to perform? (input the exact name, case sensitive)", choices=agent_names, default=agent_names[0])
+    if user_role not in agent_names:
+        return 
     user_index = agent_names.index(user_role)
-    ctx = agi_init(agent_configs, console, config.load_openai_token(), user_index)
 
+    # set up the agents
+    # TODO: define our own "console" 
+    ctx = agi_init(agent_configs, console, openai_token, user_index)
+
+    # main loop
     actions = ["continue", "interview", "exit"]
     while True:
         instruction = {"command": "continue"}
-        action = Prompt.ask(
+        action = ask_human(
             "Pick an action to perform?", choices=actions, default=actions[0]
         )
         if action == "interview":
             robot_agent_names = list(map(lambda agent: agent.name, ctx.robot_agents))
-            robot_agent_name = Prompt.ask(
+            robot_agent_name = ask_human(
                 f"As {ctx.user_agent.name}, which agent do you want to talk to?",
                 choices=robot_agent_names,
                 default=robot_agent_names[0],
