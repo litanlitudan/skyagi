35ee1b94140c441d2b0c60cb877e5ff85ed451c1
Qi Zheng <zhengqi1987@gmail.com>
2023-05-04 20:43:46 -0400

    testing api now

--- skyagi/src/skyagi/api.py
+++ skyagi/src/skyagi/api.py
@@ -23,11 +23,16 @@ def report_error(websocket, err_msg: str):
     asyncio.run(send_ws_message(websocket, err_msg))
 
 # TODO: wrapper for Prompt.ask
-def ask_human(websocket, message: str):
-    asyncio.run(send_ws_message(websocket, message))
-    return input()
+def ask_human(websocket, message: str, choices: List[str]):
+    choice_prompt = f"{message} ({'/'.join(choices)}): "
+    asyncio.run(send_ws_message(websocket, choice_prompt))
+    res = input()
+    print(res)
+    return res
 
 
+#@serving(websocket=True)
+#def runskyagi(question: str, **kwargs):
 @serving(websocket=True)
 def runskyagi(agent_configs: List[dict], **kwargs):
     """
@@ -38,35 +43,38 @@ def runskyagi(agent_configs: List[dict], **kwargs):
 
     if len(agent_configs) <= 2:
         # TODO: return error
-        report_error(websocket, "Please config at least 2 agents, exiting")
-        return
+        return "[error] Please config at least 2 agents, exiting"
     
     agent_names = []
     for agent_config in agent_configs:
         agent_names.append(agent_config["name"])
 
     # ask for the user's role
-    user_role = ask_human("Pick which role you want to perform? (input the exact name, case sensitive)", choices=agent_names, default=agent_names[0])
+    user_role = ask_human(websocket, "Pick which role you want to perform? (input the exact name, case sensitive)", choices=agent_names).strip()
+    print("user_role:", user_role)
     if user_role not in agent_names:
-        return 
+        return "[error] Please pick a valid agent, exiting" 
     user_index = agent_names.index(user_role)
+    return "close cmd"
 
     # set up the agents
     ctx = agi_init(agent_configs, console, openai_token, user_index)
+    return "close cmd"
 
     # main loop
     actions = ["continue", "interview", "exit"]
     while True:
         instruction = {"command": "continue"}
         action = ask_human(
-            "Pick an action to perform?", choices=actions, default=actions[0]
+            websocket,
+            "Pick an action to perform?", choices=actions
         )
         if action == "interview":
             robot_agent_names = list(map(lambda agent: agent.name, ctx.robot_agents))
             robot_agent_name = ask_human(
+                websocket,
                 f"As {ctx.user_agent.name}, which agent do you want to talk to?",
-                choices=robot_agent_names,
-                default=robot_agent_names[0],
+                choices=robot_agent_names
             )
             instruction = {
                 "command": "interview",
---
+++ skyagi/src/skyagi/client.py
@@ -0,0 +1,88 @@
+import asyncio
+import os
+from typing import Dict
+
+import aiohttp
+from pydantic import BaseModel, ValidationError
+
+
+class Response(BaseModel):
+    result: str
+    error: str
+    stdout: str
+
+
+class HumanPrompt(BaseModel):
+    prompt: str
+
+agent_configs = [
+    {
+        "name": "Penny",
+        "age": 22,
+        "personality": "Outgoing, friendly, compassionate, assertive, and determined.",
+        "memories": ["I'm happy"],
+        "current_status": "Penny is at the Cheesecake Factory"
+    },
+    {
+        "name": "Leonard",
+        "age": 27,
+        "personality": "Intelligent, neurotic, kind, analytical, loyal.",
+        "memories": ["I'm unhappy"],
+        "current_status": "Leonard is at the Cheesecake Factory"
+    },
+    {
+        "name": "Sheldon",
+        "age": 27,
+        "personality": "Intelligent, rigid, socially challenged, quirky, and arrogant.",
+        "memories": ["N/A"],
+        "current_status": "Sheldon is at the Cheesecake Factory"
+        }
+]
+
+
+async def client(url: str, name: str, envs: Dict = {}):
+    async with aiohttp.ClientSession() as session:
+        async with session.ws_connect(f'{url}/{name}') as ws:
+            print(f'Connected to {url}/{name}.')
+
+            await ws.send_json(
+                {
+                    "agent_configs": agent_configs,
+                    "openai_key": os.environ['OPENAI_API_KEY'],
+                    "envs": envs if envs else {},
+                }
+            )
+
+            async for msg in ws:
+                if msg.type == aiohttp.WSMsgType.TEXT:
+                    if msg.data == 'close cmd':
+                        await ws.close()
+                        break
+                    else:
+                        try:
+                            response = Response.parse_raw(msg.data)
+                            print(response.result, end='')
+                        except ValidationError:
+                            try:
+                                prompt = HumanPrompt.parse_raw(msg.data)
+                                answer = input(prompt.prompt + '\n')
+                                print("answer: ", answer)
+                                await ws.send_str(answer)
+                            except ValidationError:
+                                print(f'Unknown message: {msg.data}')
+
+                elif msg.type == aiohttp.WSMsgType.ERROR:
+                    print('ws connection closed with exception %s' % ws.exception())
+                else:
+                    print(msg)
+
+
+asyncio.run(
+    client(
+        url='ws://localhost:8080',
+        name='runskyagi',
+        envs={
+            'OPENAI_API_KEY': os.environ['OPENAI_API_KEY'],
+        },
+    )
+)
