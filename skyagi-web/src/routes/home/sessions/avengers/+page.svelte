fc02aa6c024027fde2f016e44abdb0c3e6cb47e4
serenaqiaoo <121361687+serenaqiaoo@users.noreply.github.com>
2023-05-06 11:44:50 -0700

    [UI] home page basic layout (#72)
    * layout draft
    
    * delete some codes
    
    * add charater board skeletons
    
    ---------
    
    Co-authored-by: Serena Qiao <serenaqiao@Serenas-MacBook-Pro.local>

---
+++ skyagi-web/src/routes/home/sessions/avengers/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Chatbot from '../../../../lib/chatbot.svelte';
+	import { Heading } from 'flowbite-svelte';
+	import '../../../../app.postcss';
+</script>
+
+<div class="min-h-screen h-screen flex flex-col">
+	<Heading class="p-8" tag="h1" customSize="text-3xl">Chat: Avengers</Heading>
+
+	<div class="grid grid-cols-8 gap-32">
+		<div class="col-span-2 h-120">
+			<div class="flex flex-col gap-20">
+				<div class="h-20 border border-indigo-300 rounded-md">Me (add info about Spark here)</div>
+				<div class="h-80 border border-indigo-300 rounded-md">
+					Agents (add info about other avengers characters here)
+				</div>
+			</div>
+		</div>
+		<div class="col-span-6 overflow-y-auto">
+			<Chatbot />
+		</div>
+	</div>
+</div>
