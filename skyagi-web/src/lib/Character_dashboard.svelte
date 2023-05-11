5f46be9e9f73d63911d8b195b8ea8490c0f5b137
Langxuan Su <langxuan.su@duke.edu>
2023-05-10 21:48:47 -0700

    create character dashboard

---
+++ skyagi-web/src/lib/Character_dashboard.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { CharacterType } from './types';
+	import Character from './Character.svelte';
+	import { Card } from 'flowbite-svelte';
+
+	// Define the list of characters
+	export let characters: CharacterType[];
+</script>
+
+{#each characters as character}
+	<Character {character} />
+{/each}
+
+<style>
+</style>
---
+++ skyagi-web/src/lib/types.ts
@@ -0,0 +1,5 @@
+export interface Character {
+    image: string;
+    title: string;
+    description: string;
+}
