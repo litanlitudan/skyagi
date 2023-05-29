90702f438f143c9049e5ca18d2f2686e8ca6e664
Qi Zheng <zhengqi1987@gmail.com>
2023-05-29 19:42:54 -0400

    add more telemtries

--- skyagi-web/src/routes/api/send-conversation-message/+server.ts
+++ skyagi-web/src/routes/api/send-conversation-message/+server.ts
@@ -27,7 +27,9 @@ export const PUT = (async ({ request, locals }: { request: Request; locals: App.
 	// create recipient agent
 	const agent = new GenerativeAgent(locals.supabase, conversation_id, recipient_agent_id, recipient_agent_model);
 
-	return new Response(JSON.stringify({"success": 1, "InitAgentName": initiateAgentName, "RecAgentName": agent.name, "RecAgentStatus": agent.status, "RecAgentMemLen": agent.memories.length}), { status: 200 });
+	return new Response(JSON.stringify({"success": 1, "InitAgentName": initiateAgentName, "RecAgentName": agent.name, "RecAgentStatus": agent.status}), { status: 200 });
+	//return new Response(JSON.stringify({"success": 1, "InitAgentName": initiateAgentName, "RecAgentName": agent.name, "RecAgentStatus": agent.status, "RecAgentMemLen": agent.memories.length}), { status: 200 });
+
 
 	// get reaction
 	const newMessage = `${initiateAgentName} says ${message}`;
