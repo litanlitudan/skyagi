abfe3b97bee8fd56a9d7231b7fd3c5ebc554a0a3
Qi Zheng <zhengqi1987@gmail.com>
2023-05-29 23:42:07 -0400

    update

--- skyagi-web/src/routes/api/send-conversation-message/+server.ts
+++ skyagi-web/src/routes/api/send-conversation-message/+server.ts
@@ -28,8 +28,7 @@ export const PUT = (async ({ request, locals }: { request: Request; locals: App.
     const { data: allMemories } = await locals.supabase
         .from('memory')
 		.select('id, content, metadata')
-		.contains('metadata', {"cur_status": "home"});
-		//.eq('metadata->conversation_id', conversation_id);
+		.contains('metadata',{"conversation_id": conversation_id});
 		//.eq('metadata:agent_id', recipient_agent_id)
         //.order('metadata:create_time', { ascending: true });
 	return new Response(JSON.stringify({"success": 1, "memory": allMemories}), { status: 200 });
