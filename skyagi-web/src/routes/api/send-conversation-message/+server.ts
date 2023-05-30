b43d0ba41c7d2b1f601a573c3f082157447fd398
Qi Zheng <zhengqi1987@gmail.com>
2023-05-30 00:11:02 -0400

    who calls length

--- skyagi-web/src/routes/api/send-conversation-message/+server.ts
+++ skyagi-web/src/routes/api/send-conversation-message/+server.ts
@@ -40,10 +40,10 @@ export const PUT = (async ({ request, locals }: { request: Request; locals: App.
 	const agent = new GenerativeAgent();
 	await agent.setup(locals.supabase, conversation_id, recipient_agent_id, recipient_agent_model);
 	const mem = agent.getAgentMemories(locals.supabase, conversation_id, recipient_agent_id);
-	//return new Response(JSON.stringify({"success": 1, "mem": mem}), { status: 200 });
+	return new Response(JSON.stringify({"success": 1, "mem": mem}), { status: 200 });
 	//return new Response(JSON.stringify({"success": 1, "InitAgentName": initiateAgentName, "RecAgentName": agent.name}), { status: 200 });
 	//return new Response(JSON.stringify({"success": 1, "InitAgentName": initiateAgentName, "RecAgentName": agent.name, "RecAgentStatus": agent.status, "RecAgentMemLen": agent.memories.length}), { status: 200 });
-	return new Response(JSON.stringify({"success": 1}), { status: 200 });
+	//return new Response(JSON.stringify({"success": 1}), { status: 200 });
 
 
 
