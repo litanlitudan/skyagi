fafc4aeb1bc656a6df849aafbc941a8e3ab4123d
Qi Zheng <zhengqi1987@gmail.com>
2023-05-31 17:19:31 -0400

    generateRspn working. try addMemory

--- skyagi-web/src/routes/api/send-conversation-message/+server.ts
+++ skyagi-web/src/routes/api/send-conversation-message/+server.ts
@@ -38,7 +38,7 @@ export const PUT = (async ({ request, locals }: { request: Request; locals: App.
 
 	const fullResult = await agent.generateRspn(newMessage, callToActionTemplate);
 	const result = fullResult.trim().split('\n')[0];
-	return new Response(JSON.stringify({"success": 1, "result": result}), { status: 200 });
+	//return new Response(JSON.stringify({"success": 1, "result": result}), { status: 200 });
 
 	var respMsg: string = "";
 	var ifContinue: boolean = false;
