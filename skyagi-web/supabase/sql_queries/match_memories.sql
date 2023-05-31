d87e0a26af175d0f68db3fd812242675b1686d27
Qi Zheng <zhengqi1987@gmail.com>
2023-05-31 02:23:54 -0400

    new func in supabase

--- skyagi-web/supabase/sql_queries/match_memories.sql
+++ skyagi-web/supabase/sql_queries/match_memories.sql
@@ -1,8 +1,8 @@
 -- Create a function to search for memories
 drop function if exists match_memories;
-create function match_memories (
-  query_embedding vector(1536),
+create or replace function match_memories (
   match_count int,
+  query_embedding vector(1536),
   filter jsonb DEFAULT '{}'
 ) returns table (
   id uuid,
@@ -16,14 +16,14 @@ as $$
 begin
   return query
   select
-    e.id,
-    e.content,
-    e.metadata,
-    ((1 - 0.01) ^ EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP - e.metadata->>'last_access_time')::timestamp)/3600)
-      + (e.metadata->>'importance')::float
-      + (1 - (e.embedding <=> query_embedding)) AS score
-  from memory AS e
-  where metadata @> filter
+    id,
+    content,
+    metadata,
+    (power((1 - 0.01), EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP - (to_timestamp(metadata->>'last_access_time', 'YYYY-MM-DDTHH24:MI:SS.MS') AT TIME ZONE 'America/Los_Angeles')))/3600))
+      + (metadata->>'importance')::float
+      + (1 - (embedding <=> query_embedding)) AS score
+  from memory
+  where metadata @> filter 
   order by score desc
   limit match_count;
 end;
