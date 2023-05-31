8dbf2583ce2f652611202ba32d38ae03058cd01c
Zhehao Qiao <qiaozhehao951115@gmail.com>
2023-05-31 06:27:01 -0700

    added search bar

--- skyagi-web/src/routes/room/new/+page.svelte
+++ skyagi-web/src/routes/room/new/+page.svelte
@@ -1,1 +1,9 @@
+<script>
+import { Search } from 'flowbite-svelte'
+
+
+</script>
 <h1 class="text-4xl">Create new chat room</h1>
+<Search size="md" placeholder="abc">
+    <button>Search</button>
+</Search>
