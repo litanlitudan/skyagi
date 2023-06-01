c76bf5e54e27c382b546164b789ad0891f14ddf5
Zhehao Qiao <qiaozhehao951115@gmail.com>
2023-06-01 01:44:37 -0700

    character layout mostly done

--- skyagi-web/src/lib/room-new-character.svelte
+++ skyagi-web/src/lib/room-new-character.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="container">
-    <input type=checkbox checked={yes}>
+    <input type=checkbox checked={yes} class="characterCheck">
     <figure>
         <img src={character.image} alt="" width=100px />
         <figcaption> {character.name} </figcaption>
@@ -14,11 +14,18 @@
 <style>
     figure {
         text-align: center;
+        width: 100px;
+        transform: translateX(-50px);
     }
     .container {
-        grid-template-columns: 20px 200px;
+        grid-template-columns: 20px 100px;
         column-gap: 0px;
         display: grid;
         align-items: center;
+    }
+    .characterCheck {
+        width:20px;
+        margin: 0px;
+        transform: translateX(-20px);
     }
 </style>
--- skyagi-web/src/routes/room/new/+page.svelte
+++ skyagi-web/src/routes/room/new/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-    import { Search } from 'flowbite-svelte';
     // import Scrolly from "./Scrolly.svelte";
     import Character from '$lib/room-new-character.svelte';
-	import CharacterDashboard from '$lib/Character_dashboard.svelte';
     export const characters = [
         {name: "tan li", image: "../src/lib/assets/Avatar1.png", title:"", description:""},
         {name: "yy", image: "../src/lib/assets/Avatar2.png", title:"", description:""},
@@ -16,26 +14,62 @@
         let widthY = 200;
         let leftVal = (inputVal%3) * widthX+startPointX
         let topVal = Math.floor(inputVal/3) * widthY+startPointY
-        console.log("positin:absolute;left:" + leftVal.toString() + "px;top:" + topVal.toString() + "px") 
-        return "position:absolute;left:" + leftVal.toString() + "px;top:" + topVal.toString() + "px"
+        // console.log("positin:absolute;left:" + leftVal.toString() + "px;top:" + topVal.toString() + "px") 
+        return "position:static;left:" + leftVal.toString() + "px;top:" + topVal.toString() + "px"
     }
+
+    function get2DCharacterLs(inputLs, colNum) {
+        let rstLs = []
+        for (let i=0; i<Math.ceil(inputLs.length/colNum); i++){
+            rstLs.push([]);
+        }
+        for (let i=0; i<inputLs.length;i++){
+            rstLs[Math.floor(i/3)].push(inputLs[i]);
+        }
+        return rstLs
+    }
+    export const characters2D = get2DCharacterLs(characters, 3);
 
 </script>
 <input id="searchBar" type="search" placeholder="Search..." />
-{#each characters as character, i}
-    <div class="characterInfoSet" style={getNthPicPositionStr(i)}>
-    <Character {character}>
 
-    </Character>
-    </div>
-{/each}
 
+<!-- <div class="scroller">
+    {#each characters2D as characterLs, i}
+        <div class="characterRowSet">
+        {#each characterLs as character, j}
+            <div class="characterInfoSet" style={getNthPicPositionStr(i)}>
+                <Character {character}>
+                </Character>
+            </div>
+        {/each}
+        </div>
+    {/each}
+</div> -->
+<div class="scroller">
+    {#each characters as character, i}
+        <div class="characterInfoSet" style={getNthPicPositionStr(i)}>
+            <Character {character}>
+            </Character>
+        </div>
+    {/each}
+</div>
 <style>
     #searchBar {
         width: 200px
     }
-    /* .characterInfoSet {
 
-     } */
+    .characterRowSet {
+        width: 100px;
+    }
 
+    .scroller {
+        width: 700px;
+        height: 500px;
+        overflow-x: scroll;
+        overflow-y: scroll;
+        display: grid;
+        grid-template-columns: repeat(3, 180px);
+        grid-template-rows: repeat(auto-fill, 200px);
+}
 </style>
