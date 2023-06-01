750bbfc7abc4f3e345285e35a0c254bf83910c67
Qi Zheng <zhengqi1987@gmail.com>
2023-06-01 18:14:17 -0400

    still need to calculate embeddings and importance

--- skyagi-web/src/routes/api/create-conversation/+server.ts
+++ skyagi-web/src/routes/api/create-conversation/+server.ts
@@ -8,5 +8,58 @@ export const config: Config = {
 };
 
 export const PUT = (async ({ request, locals }: { request: Request; locals: App.Locals }) => {
+	const {
+		name,
+		user_id,
+		agents,
+		user_agents
+	} = await request.json();
+
+	// create conversation
+	const res = await locals.supabase
+		.from('conversation')
+		.insert({
+			user_id: user_id,
+			name: name,
+			agents: agents,
+			user_agents: user_agents
+		});
+
+	// assumption: (user_id, name) combo is unique
+	const { data: conv_id } = await locals.supabase
+		.from('conversation')
+		.select('id')
+		.eq('user_id', user_id)
+		.eq('name', name);
+
+	// get agents' initial memory and add to memory
+	const currentTime = new Date().toISOString();
+	for (const agent of agents) {
+		const agent_info = await locals.supabase
+			.from('agent')
+			.select('initial_status, initial_memory')
+			.eq('id', agent);
+		
+		const embedding = ;
+		const importance = ;
+		const metadata = {
+			agent_id: agent,
+			cur_status: agent_info[0].initial_status,
+			importance: importance,
+			create_time: currentTime,
+			conversation_id: conv_id[0].id,
+			last_access_time: currentTime
+		};
+
+		await locals.supabase
+			.from('memory')
+			.insert({
+				content: agent_info[0].initial_memory,
+				embedding: embedding,
+				metadata: metadata
+			});
+	}
+
+	// need to call add_memory?
 	return new Response(JSON.stringify({ message: 'Success' }), { status: 200 });
 }) satisfies RequestHandler;
