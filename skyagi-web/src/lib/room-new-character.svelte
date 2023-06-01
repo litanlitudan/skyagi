d807d9fb97b944d5d84eb6f998e8936781758a02
Zhehao Qiao <qiaozhehao951115@gmail.com>
2023-05-31 23:02:32 -0700

    room page added character component

---
+++ skyagi-web/src/lib/room-new-character.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { CharacterType } from './types';
+	export let character: CharacterType;
+    let yes=false;
+</script>
+
+<div class="container">
+    <input type=checkbox checked={yes}>
+    <figure>
+        <img src={character.image} alt="" width=100px />
+        <figcaption> {character.name} </figcaption>
+    </figure>
+</div>
+<style>
+    figure {
+        text-align: center;
+    }
+    .container {
+        grid-template-columns: 20px 200px;
+        column-gap: 0px;
+        display: grid;
+        align-items: center;
+    }
+</style>
--- skyagi-web/src/routes/room/new/+page.svelte
+++ skyagi-web/src/routes/room/new/+page.svelte
@@ -1,9 +1,41 @@
-<script>
-import { Search } from 'flowbite-svelte'
+<script lang="ts">
+    import { Search } from 'flowbite-svelte';
+    // import Scrolly from "./Scrolly.svelte";
+    import Character from '$lib/room-new-character.svelte';
+	import CharacterDashboard from '$lib/Character_dashboard.svelte';
+    export const characters = [
+        {name: "tan li", image: "../src/lib/assets/Avatar1.png", title:"", description:""},
+        {name: "yy", image: "../src/lib/assets/Avatar2.png", title:"", description:""},
+        {name: "Vegeta", image: "../src/lib/assets/Avatar3.png", title:"", description:""},
+        {name: "Goku", image:"../src/lib/assets/Avatar3.png", title:"", description:""}];
 
+    function getNthPicPositionStr(inputVal) {
+        let startPointX = 120;
+        let startPointY = 150;
+        let widthX = 200;
+        let widthY = 200;
+        let leftVal = (inputVal%3) * widthX+startPointX
+        let topVal = Math.floor(inputVal/3) * widthY+startPointY
+        console.log("positin:absolute;left:" + leftVal.toString() + "px;top:" + topVal.toString() + "px") 
+        return "position:absolute;left:" + leftVal.toString() + "px;top:" + topVal.toString() + "px"
+    }
 
 </script>
-<h1 class="text-4xl">Create new chat room</h1>
-<Search size="md" placeholder="abc">
-    <button>Search</button>
-</Search>
+<input id="searchBar" type="search" placeholder="Search..." />
+{#each characters as character, i}
+    <div class="characterInfoSet" style={getNthPicPositionStr(i)}>
+    <Character {character}>
+
+    </Character>
+    </div>
+{/each}
+
+<style>
+    #searchBar {
+        width: 200px
+    }
+    /* .characterInfoSet {
+
+     } */
+
+</style>
