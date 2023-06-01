880a9a30e54545c88701a4d7787680edd7c7bce0
Kejie Zhang <kejiezhang000@gmail.com>
2023-05-31 21:33:58 -0600

    feat: python runtime setup

---
+++ skyagi-web/api/index.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/api/hello")
+async def hello():
+    return {"message": "Hello world!"}
---
+++ skyagi-web/src/routes/hello/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	let message = '';
+
+	async function hello() {
+		const response = await fetch('/api/hello', {
+			method: 'GET',
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+
+		let data = await response.json();
+		message = data['message'];
+	}
+</script>
+
+<button on:click={hello}>Hello</button>
+<p>{message}</p>
