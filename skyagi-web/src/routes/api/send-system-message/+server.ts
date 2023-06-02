d121ae5b43a34f79d31b791a7f7d34ed41595e8a
Qi <zhengqi1987@gmail.com>
2023-06-02 14:06:50 -0700

    [api] impl send-conversation-message and add a few endpoint placeholders (#91)
    * Add API folder
    
    * add placeholders for more apis
    
    * fix formats
    
    * temp check-in
    
    * add agent lib
    
    * add generateRspn methods
    
    * add addMemory
    
    * follow JS/TS naming convention
    
    * add langchain
    
    * add for debug
    
    * get authtoken from cookie
    
    * FORBIDDEN MOVE
    
    * try supabase access
    
    * add the actual model for now
    
    * handle async
    
    * fix more wainings
    
    * try supabase agent table
    
    * debugging
    
    * continue debugging
    
    * continue debugging
    
    * continue debugging
    
    * continue debugging
    
    * debug supabase agent table
    
    * debug agent initialization
    
    * debug memory table later
    
    * don't read id from agent table
    
    * constructor can't be async
    
    * debug
    
    * debug vectorstore debug
    
    * half-way done using the new retriever
    
    * don't use global memories as the way to get mem slices
    
    * use global memory to handle slicing
    
    * add more telemtries
    
    * add more telemtries
    
    * add more telemtries
    
    * add more telemtries
    
    * write async setup for class
    
    * debug db
    
    * debug db
    
    * debug db
    
    * debug db
    
    * debug db
    
    * debug db
    
    * debug db
    
    * debug db
    
    * debug db
    
    * debug db
    
    * debug memory table access
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * test ordering
    
    * test status
    
    * test status
    
    * test ordering
    
    * debug getmemory
    
    * who calls length
    
    * who calls length
    
    * who calls length
    
    * who calls length
    
    * who calls length
    
    * who calls length
    
    * who calls length
    
    * who calls length
    
    * who calls length
    
    * who calls length
    
    * who calls length
    
    * who calls length
    
    * who calls length
    
    * who calls length
    
    * who calls length
    
    * who calls length
    
    * who calls length
    
    * creat the agent successfully
    
    * clear up async calls
    
    * try generaterspn
    
    * add test functions to test adddoc and getrelevantdoc
    
    * test relevance
    
    * fix sql
    
    * debug create agent again with the new sql function
    
    * debug getrelevant
    
    * new func in supabase
    
    * add filter
    
    * debug filter using the vs directly
    
    * add the filter to vs similaritysearch
    
    * change the syntax
    
    * change the syntax
    
    * use metdata subfield only
    
    * vs working. try retriever now
    
    * try reduce k to 1
    
    * filter working. delete tmp debugging vs
    
    * continue debug generaterspn as retriever is working
    
    * fix the self-maintained metadata
    
    * debug getNumTokens
    
    * prompt.format is async
    
    * debug action prediction chain
    
    * continue debug actionPredictionChain
    
    * correct the initiate agent name
    
    * try generateRspn again because kwargs looks good
    
    * debug where the issue is
    
    * dry run
    
    * try a different prompt
    
    * get gateway timeout
    
    * get gateway timeout
    
    * get gateway timeout
    
    * get gateway timeout
    
    * use call instead of run
    
    * debug result
    
    * should work. but could hit timeout
    
    * generateRspn working. try addMemory
    
    * addmemory working
    
    * debug adding to the message table
    
    * debug adding to the message table
    
    * debug message table insert
    
    * debug specifically the insert
    
    * debug specifically the insert
    
    * add conversation to the message table
    
    * add conversation to the message table
    
    * clean up TODO
    
    * clean up todos
    
    * delete two debug helper functions
    
    * cache supabase
    
    * use the cached storage
    
    * add updatememoryaccesstime
    
    * figure out the db part
    
    * debug table update
    
    * use single quote
    
    * try to use variables
    
    * try to use variables
    
    * use something existing
    
    * try the fixed version
    
    * cleanup
    
    * delete unused file
    
    * remove testing hack
    
    * order should be desc

---
+++ skyagi-web/src/routes/api/send-system-message/+server.ts
@@ -0,0 +1,12 @@
+import { error } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import type { Config } from '@sveltejs/adapter-vercel';
+
+// Can switch to the edge func if serverless is not necessary
+export const config: Config = {
+	runtime: 'nodejs18.x'
+};
+
+export const PUT = (async ({ request, locals }: { request: Request; locals: App.Locals }) => {
+	return new Response(JSON.stringify({ message: 'Success' }), { status: 200 });
+}) satisfies RequestHandler;
