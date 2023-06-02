32bd5da26d537746537db7122b88d0b298ca118a
Qi Zheng <zhengqi1987@gmail.com>
2023-06-02 19:26:25 -0400

    update get-agents

--- skyagi-web/src/routes/api/get-agents/+server.ts
+++ skyagi-web/src/routes/api/get-agents/+server.ts
@@ -1,6 +1,5 @@
 import type { RequestHandler } from './$types';
 import type { Config } from '@sveltejs/adapter-vercel';
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { checkValidity } from '$lib/utils';
 
 
@@ -11,71 +10,17 @@ export const config: Config = {
 
 export const PUT = (async ({ request, locals }: { request: Request; locals: App.Locals }) => {
 	const {
-		name,
-		user_id,
-		agent_ids,
-		user_agent_ids
+		user_id
 	} = await request.json();
 
-	// create conversation
-	const res = await locals.supabase
-		.from('conversation')
-		.insert({
-			user_id: user_id,
-			name: name,
-			agents: agent_ids,
-			user_agents: user_agent_ids
-		});
+	const { data: agents } = await locals.supabase
+		.from('agent')
+		.select('id, name, age, personality, initial_status, initial_memory')
+		.eq('user_id', user_id);
 
-	// assumption: (user_id, name) combo is unique
-	const { data: conv_id } = await locals.supabase
-		.from('conversation')
-		.select('id')
-		.eq('user_id', user_id)
-		.eq('name', name);
-
-	if (checkValidity(conv_id) === false) {
-		return new Response(JSON.stringify({ 'success': 0, 'error': 'failed to find conversation' }), { status: 200 });
-	}
-	
-	// get agents' initial memory and add to memory
-    const embeddings = new OpenAIEmbeddings();
-	const currentTime = new Date().toISOString();
-
-	for (const agent_id of agent_ids) {
-		const { data: agent_info } = await locals.supabase
-			.from('agent')
-			.select('initial_status, initial_memory')
-			.eq('id', agent_id);
-
-	    if (checkValidity(agent_info) === false) {
-		    return new Response(JSON.stringify({ 'success': 0, 'error': 'agent not found' }), { status: 200 });
-	    }
-
-		const embedding = await embeddings.embedQuery(agent_info[0].initial_memory);
-		const importance = 0.0;
-		const metadata = {
-			agent_id: agent_id,
-			cur_status: agent_info[0].initial_status,
-			importance: importance,
-			create_time: currentTime,
-			conversation_id: conv_id[0].id,
-			last_access_time: currentTime
-		};
-
-		const { error } = await locals.supabase
-			.from('memory')
-			.insert({
-				content: agent_info[0].initial_memory,
-				embedding: embedding,
-				metadata: metadata
-			});
-		
-		if (error) {
-			return new Response(JSON.stringify({ 'success': 0, 'error': 'failed to create new initial memory' }), { status: 200 });
-		}
-	}
-
-	// need to call add_memory?
-	return new Response(JSON.stringify({ success: 1, conversation_id: conv_id[0].id }), { status: 200 });
+	if (checkValidity(agents) === false) {
+		return new Response(JSON.stringify({ 'success': 0, 'error': 'agent not found' }), { status: 200 });
+	} else {
+        return new Response(JSON.stringify({ 'success': 1, 'agents': agents }), { status: 200 });
+    }
 }) satisfies RequestHandler;
