0d4905a59efe9d22c49f7bd7bc128c98668e2b30
Zhehao Qiao <qiaozhehao951115@gmail.com>
2023-06-02 01:53:13 -0700

    editing user interface, wip interaction between left side and right side

--- skyagi-web/src/lib/room-new-character.svelte
+++ skyagi-web/src/lib/room-new-character.svelte
@@ -5,7 +5,9 @@
 </script>
 
 <div class="container">
-    <input type=checkbox checked={yes} class="characterCheck">
+    <div class=characterCheck>
+        <input type=checkbox checked={yes} class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+    </div>
     <figure>
         <img src={character.image} alt="" width=100px />
         <figcaption> {character.name} </figcaption>
@@ -15,7 +17,7 @@
     figure {
         text-align: center;
         width: 100px;
-        transform: translateX(-50px);
+        /* transform: translateX(50px); */
     }
     .container {
         grid-template-columns: 20px 100px;
@@ -24,8 +26,8 @@
         align-items: center;
     }
     .characterCheck {
-        width:20px;
+        /* width:20px; */
         margin: 0px;
-        transform: translateX(-20px);
+        transform: translateX(-5px);
     }
 </style>
--- skyagi-web/src/routes/room/new/+page.svelte
+++ skyagi-web/src/routes/room/new/+page.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
     // import Scrolly from "./Scrolly.svelte";
     import Character from '$lib/room-new-character.svelte';
+    import { Select, Label } from 'flowbite-svelte';
     export const characters = [
         {name: "tan li", image: "../src/lib/assets/Avatar1.png", title:"", description:""},
         {name: "yy", image: "../src/lib/assets/Avatar2.png", title:"", description:""},
         {name: "Vegeta", image: "../src/lib/assets/Avatar3.png", title:"", description:""},
         {name: "Goku", image:"../src/lib/assets/Avatar3.png", title:"", description:""}];
+    let lastClickedCharacter = "..."
+
+
 
     function getNthPicPositionStr(inputVal) {
         let startPointX = 120;
@@ -30,46 +34,92 @@
     }
     export const characters2D = get2DCharacterLs(characters, 3);
 
+    let selectedModel;
+    let models = [
+		{ value: 'openai-gpt-3.5-turbo', name: 'openai-gpt-3.5-turbo' },
+		{ value: 'openai-gpt-4', name: 'openai-gpt-4' }
+		// { value: 'chatglm-6b-modelz', name: 'chatglm-6b-modelz' },
+		// { value: 'moss-16b-modelz', name: 'moss-16b-modelz' },
+		// { value: 'vicuna-13b-modelz', name: 'vicuna-13b-modelz' },
+		// { value: 'mpt-7b-modelz', name: 'mpt-7b-modelz' }
+	];
 </script>
-<input id="searchBar" type="search" placeholder="Search..." />
 
-
-<!-- <div class="scroller">
-    {#each characters2D as characterLs, i}
-        <div class="characterRowSet">
-        {#each characterLs as character, j}
-            <div class="characterInfoSet" style={getNthPicPositionStr(i)}>
+<div id="globalGrid">
+    
+    <div>
+        <input id="searchBar" type="search" placeholder="Search..." />
+    </div>
+    
+    <div class="scroller">
+        {#each characters as character, i}
+            <div class="characterInfoSet">
                 <Character {character}>
                 </Character>
             </div>
         {/each}
-        </div>
-    {/each}
-</div> -->
-<div class="scroller">
-    {#each characters as character, i}
-        <div class="characterInfoSet" style={getNthPicPositionStr(i)}>
-            <Character {character}>
-            </Character>
-        </div>
-    {/each}
+    </div>
+
+    <input id="chatNameField" placeholder="Chat name">
+
+    <div>
+        <h1>
+            Select Model for {lastClickedCharacter}
+        </h1>
+        <Label>Select an option
+            <Select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" items={models} bind:value={selectedModel}
+            placeholder = "Select LLM" />
+        </Label>
+
+        <h1>
+            API Key
+        </h1>
+        <input id="APIKeyField" placeholder="Input key">
+
+        <h1>
+            You will play...
+        </h1>
+        <Label class="mb-10 w-1/2">Select an option
+            <Select id="modelDropDown" class="mt-5" size="lg" items={models} bind:value={selectedModel}
+            placeholder = "Select LLM" />
+        </Label>
+        <button>
+            Create
+        </button>
+
+    </div>
 </div>
+
+
 <style>
     #searchBar {
         width: 200px
     }
 
-    .characterRowSet {
-        width: 100px;
+    #chatNameField {
+        width: 200px
     }
 
+
     .scroller {
-        width: 700px;
+        width: 600px;
         height: 500px;
+        top: 20px;
+        position: relative;
         overflow-x: scroll;
         overflow-y: scroll;
         display: grid;
-        grid-template-columns: repeat(3, 180px);
-        grid-template-rows: repeat(auto-fill, 200px);
-}
+        grid-template-columns: repeat(3, 200px);
+        grid-template-rows: repeat(auto-fill, 220px);
+    }
+
+    #globalGrid {
+        display: grid;
+        grid-template-rows: repeat(2, 50px);
+        grid-auto-flow: column;
+        gap: 10px;
+        /* grid-auto-columns: 400px 400px; */
+    }
+
+
 </style>
