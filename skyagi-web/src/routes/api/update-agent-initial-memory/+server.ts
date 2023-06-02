c86a429793a0d9ff4b8b9849e5efae15b7304424
Qi Zheng <zhengqi1987@gmail.com>
2023-06-02 18:58:06 -0400

    update update-agent-initial-memory

--- skyagi-web/src/routes/api/update-agent-initial-memory/+server.ts
+++ skyagi-web/src/routes/api/update-agent-initial-memory/+server.ts
@@ -1,6 +1,6 @@
-import { error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import type { Config } from '@sveltejs/adapter-vercel';
+import { checkValidity } from '$lib/utils';
 
 // Can switch to the edge func if serverless is not necessary
 export const config: Config = {
@@ -14,36 +14,48 @@ export const PUT = (async ({ request, locals }: { request: Request; locals: App.
 	} = await request.json();
 
 	// get memories
-	const agent_memories = await locals.supabase
+	const { data: agent_memories} = await locals.supabase
 		.from('memory')
 		.select('content')
-		.eq('metadata->agent_id', agent_id)
-		.eq('metadata->conversation_id', conversation_id);
-	const memories = "";
+		.contains('metadata', {'agent_id': agent_id})
+		.contains('metadata', {'conversation_id': conversation_id});
+
+	if (checkValidity(agent_memories) === false) {
+		return new Response(JSON.stringify({ 'success': 0, 'error': 'failed loading agent memories' }), { status: 200 });
+	}
+
+	let memories = "";
 	for (const mem of agent_memories) {
-		memories += mem.content;
+		memories += (mem.content + "\n");
 	}
 
 	// get existing agent
-	const existing_agent = await locals.supabase
+	const { data: existing_agent } = await locals.supabase
 		.from('agent')
 		.select('user_id, name, age, personality, initial_status')
 		.eq('id', agent_id);
 
+	if (checkValidity(existing_agent) === false) {
+		return new Response(JSON.stringify({ 'success': 0, 'error': 'agent not found' }), { status: 200 });
+	}
+
     // update the memory	
-	const res = await locals.supabase
+	const { error } = await locals.supabase
 		.from('agent')
 		.update({
 			user_id: existing_agent[0].user_id,
 			name: existing_agent[0].name,
 		    age: existing_agent[0].age,
 			personality: existing_agent[0].personality,
-			initial_status: existing_agent[0].status,
+			initial_status: existing_agent[0].initial_status,
 			initial_memory: memories
 		})
 		.eq('id', agent_id);
 
-
-	return new Response(JSON.stringify({ message: res }), { status: 200 });
+	if (error) {
+		return new Response(JSON.stringify({ 'success': 0, 'error': "failed to update agent initial memory" }), { status: 200 });
+	} else {
+		return new Response(JSON.stringify({ 'success': 1 }), { status: 200 });
+	}
 
 }) satisfies RequestHandler;
