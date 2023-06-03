63ccd36084e43b472f5ec27ec1560297053473d7
Qi <zhengqi1987@gmail.com>
2023-06-02 20:30:21 -0700

    [api] add more endpoints (#94)
    * add create-agent endpoint
    
    * add incubate-agent
    
    * add update-agent-initial-memory
    
    * still need to calculate embeddings and importance
    
    * add create-conversation ep
    
    * add debug hack
    
    * fix issue caused by merge conflict
    
    * debug create-agent
    
    * debug retrieval of the ID
    
    * debug retrieval of the ID
    
    * change the retrieval filter
    
    * return error
    
    * return error
    
    * util func to check data validity
    
    * debug initial_memory
    
    * fix agent info read
    
    * make create-conversation field more self-explanatory
    
    * update incubate-agent
    
    * add failed reasons
    
    * use contain for jsonb filter & return failure reason
    
    * use var for memory collection
    
    * split memories with new line
    
    * update update-agent-initial-memory
    
    * Revert "add debug hack"
    
    This reverts commit 4fe313844d5b994300fcb5c14409e5980b60d9a8.
    
    * cleanup
    
    * fix some indents

--- skyagi-web/src/routes/api/create-agent/+server.ts
+++ skyagi-web/src/routes/api/create-agent/+server.ts
@@ -1,6 +1,6 @@
-import { error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import type { Config } from '@sveltejs/adapter-vercel';
+import { checkValidity } from '$lib/utils';
 
 // Can switch to the edge func if serverless is not necessary
 export const config: Config = {
@@ -8,5 +8,35 @@ export const config: Config = {
 };
 
 export const PUT = (async ({ request, locals }: { request: Request; locals: App.Locals }) => {
-	return new Response(JSON.stringify({ message: 'Success' }), { status: 200 });
+	const {
+		user_id,
+		agent
+	} = await request.json();
+
+	const res = await locals.supabase
+		.from('agent')
+		.insert({
+			user_id: user_id,
+			name: agent.name,
+		    age: agent.age,
+			personality: agent.personality,
+			initial_status: agent.status,
+			initial_memory: agent.memory
+		});
+
+	const { data } = await locals.supabase
+		.from('agent')
+		.select('id')
+		.eq('user_id', user_id)
+		.eq('age', agent.age)
+		.eq('name', agent.name)
+		.eq('personality', agent.personality)
+		.eq('initial_status', agent.status)
+		.eq('initial_memory', agent.memory);
+	
+	if (checkValidity(data)) {
+		return new Response(JSON.stringify({ 'success': 1, agent_id: data[0].id }), { status: 200 });
+	} else {
+		return new Response(JSON.stringify({ 'success': 0, 'error': 'failed to create new agent' }), { status: 200 });
+	}
 }) satisfies RequestHandler;
