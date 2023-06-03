fd676238f172908fb5c47615144a7bccb57570f3
Kejie Zhang <kejiezhang000@gmail.com>
2023-06-03 13:54:36 -0700

    fix: delete agent

--- skyagi-web/src/lib/AgentDetails.svelte
+++ skyagi-web/src/lib/AgentDetails.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { isAgentFormEditing } from './stores';
 	import type { AgentDataType } from './types';
-	import { Button } from 'flowbite-svelte';
+	import { Button, Modal } from 'flowbite-svelte';
 
 	export let agentData: AgentDataType;
+
+	let popupDeleteModal = false;
 
 	function handleEdit() {
 		isAgentFormEditing.set(true);
@@ -21,6 +24,8 @@
 			method: 'PUT',
 			body: JSON.stringify({ agent_id, user_id })
 		});
+
+		goto('/agent');
 	};
 </script>
 
@@ -41,8 +46,32 @@
 			</ul>
 		{/if}
 		<Button type="button" on:click={handleEdit}>Edit</Button>
-		<Button type="button" color="red" on:click={handleDelete}>Delete</Button>
+		<Button type="button" color="red" on:click={() => (popupDeleteModal = true)}>Delete</Button>
 	{:else}
 		<p>Agent not found</p>
 	{/if}
+
+	<Modal bind:open={popupDeleteModal} size="xs" autoclose>
+		<div class="text-center">
+			<svg
+				aria-hidden="true"
+				class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200"
+				fill="none"
+				stroke="currentColor"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+				/></svg
+			>
+			<h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+				Are you sure you want to delete this agent?
+			</h3>
+			<Button color="red" class="mr-2" on:click={handleDelete}>Yes, I'm sure</Button>
+			<Button color="alternative">No, cancel</Button>
+		</div>
+	</Modal>
 </main>
