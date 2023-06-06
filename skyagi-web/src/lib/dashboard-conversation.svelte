978ac9c36401ab67dde363469c3353a7e2c5967c
Fred Qiao <qiaozhehao951115@gmail.com>
2023-06-05 22:16:33 -0700

    [web] add dashboard UI (#100)
    * added temp conversation dashboard
    
    * dashboard basic layout

---
+++ skyagi-web/src/lib/dashboard-conversation.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { AccordionItem, Accordion } from 'flowbite-svelte';
+    export let conversationIndex;
+    export let conversationText;
+    let open = false;
+    if (conversationIndex == 1) {
+        open = true;
+    }
+</script>
+
+<AccordionItem open={open}>
+<span slot="header">Conversation {conversationIndex}</span>
+    <p class="mb-2 text-gray-500 dark:text-gray-400">{conversationText}</p>
+</AccordionItem>
