81604f58151c345c677dc7ab94e5bdd7790c118c
Qi Zheng <zhengqi1987@gmail.com>
2023-06-06 17:26:49 -0400

    start over

--- skyagi-web/src/routes/+page.server.ts
+++ skyagi-web/src/routes/+page.server.ts
@@ -6,11 +6,9 @@ export const load: PageServerLoad = async ({ url, locals: { getSession } }) => {
 	const session = await getSession();
 
 	// if the user is already logged in return them to the account page
-	/*
 	if (session) {
-		throw redirect(303, '/dashboard');
+		throw redirect(303, '/account');
 	}
-	*/
 
 	return { url: url.origin };
 };
