31b2a09916cfc1e787fdda367715b29d2086581c
Qi Zheng <zhengqi1987@gmail.com>
2023-06-06 17:15:31 -0400

    no redirect in page.server.ts

--- skyagi-web/src/routes/+page.server.ts
+++ skyagi-web/src/routes/+page.server.ts
@@ -6,9 +6,11 @@ export const load: PageServerLoad = async ({ url, locals: { getSession } }) => {
 	const session = await getSession();
 
 	// if the user is already logged in return them to the account page
+	/*
 	if (session) {
 		throw redirect(303, '/dashboard');
 	}
+	*/
 
 	return { url: url.origin };
 };
