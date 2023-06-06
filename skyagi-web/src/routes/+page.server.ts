8a63850a7cfb820fc9486bdaf3c5f58f04c5e8b5
Qi Zheng <zhengqi1987@gmail.com>
2023-06-06 19:06:29 -0400

    fix comments

--- skyagi-web/src/routes/+page.server.ts
+++ skyagi-web/src/routes/+page.server.ts
@@ -5,7 +5,7 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async ({ url, locals: { getSession } }) => {
 	const session = await getSession();
 
-	// if the user is already logged in return them to the account page
+	// if the user is already logged in return them to the dasnboard page
 	if (session) {
 		throw redirect(303, '/dashboard');
 	}
