617d475f9bb8095bec492e790f17cb22d32d2de7
Qi Zheng <zhengqi1987@gmail.com>
2023-06-06 17:10:03 -0400

    Revert "debug the issue"
    This reverts commit 1ad2be7d1a716e5214c7138dd91351eeed9b2b4c.

--- skyagi-web/src/routes/+page.server.ts
+++ skyagi-web/src/routes/+page.server.ts
@@ -5,10 +5,5 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async ({ url, locals: { getSession } }) => {
 	const session = await getSession();
 
-	// if the user is already logged in return them to the account page
-    if (session) {
-		throw redirect(303, '/dashboard');
-	}
-
 	return { url: url.origin };
 };
