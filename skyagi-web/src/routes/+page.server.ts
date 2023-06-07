07a82c19e7f1ff8b719227b07240e5e0df0cc8f4
Kejie Zhang <kejiezhang000@gmail.com>
2023-06-07 00:07:52 -0700

    fix: enable session

--- skyagi-web/src/routes/+page.server.ts
+++ skyagi-web/src/routes/+page.server.ts
@@ -3,12 +3,12 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ url, locals: { getSession } }) => {
-	const session = await getSession();
+    const session = await getSession();
 
-	// if the user is already logged in return them to the account page
-	if (session) {
-		throw redirect(303, '/account');
-	}
+    // if the user is already logged in return them to the dashboard page
+    if (session) {
+        throw redirect(303, '/dashboard');
+    }
 
-	return { url: url.origin };
+    return { url: url.origin };
 };
