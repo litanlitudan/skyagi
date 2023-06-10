ed57a6d01b7a1672019fb82aff958592624cacac
Tan Li <tan@tanli.dev>
2023-06-10 11:37:12 -0700

    update

--- community-profiler/tests/deploy_modal.py
+++ community-profiler/tests/deploy_modal.py
@@ -1,9 +1,20 @@
 import requests
 import zipfile
 from pathlib import Path
+from modal import Secret, Stub, Image, web_endpoint
+
+image = Image.debian_slim().pip_install(
+    "lancedb", "openai", "pandas", "tiktoken", "unstructured", "tabulate"
+)
+
+stub = Stub(
+    name="community-potential-stargazer",
+    image=image,
+    secrets=[Secret.from_name("skyagi")],
+)
 
 
-def download_db(repo="skyagi"):
+def download_db(repo: str):
     lance_db = requests.get(
         f"https://skyagi-public.s3.us-west-1.amazonaws.com/{repo}.zip"
     )
@@ -14,4 +25,4 @@ def download_db(repo="skyagi"):
     file.extractall(".")
 
 
-download_db()
+download_db("skyagi")
