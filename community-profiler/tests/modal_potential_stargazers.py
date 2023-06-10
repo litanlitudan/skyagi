0fb23035a3af59f8dfb6dabee2d3bdfff4939f2f
Tan Li <tan@tanli.dev>
2023-06-10 13:02:50 -0700

    update

--- community-profiler/tests/modal_potential_stargazers.py
+++ community-profiler/tests/modal_potential_stargazers.py
@@ -1,6 +1,15 @@
+import os
+import lancedb
 import requests
 import zipfile
+import pandas as pd
+
 from pathlib import Path
+from typing import List
+from sentence_transformers import SentenceTransformer
+from github import Github
+from github.NamedUser import NamedUser
+from tqdm import tqdm
 from modal import Secret, Stub, Image, web_endpoint
 
 image = Image.debian_slim().pip_install(
@@ -25,4 +34,103 @@ def download_db(repo: str):
     file.extractall(".")
 
 
-download_db("skyagi")
+def find_relevant_repos(query: str, db_path: Path):
+    model = SentenceTransformer("paraphrase-albert-small-v2")
+    query_embedding = model.encode([query])[0]
+
+    db = lancedb.connect(db_path)
+    tbl = db.open_table("related_repos")
+    relevant_repos = tbl.search(query_embedding).to_df()
+    relevant_repos = relevant_repos.groupby(
+        ["full_name", "created_at", "description", "stargazers_count"], as_index=False
+    )["starred_by"].count()
+    relevant_repos = relevant_repos.sort_values(
+        by=["stargazers_count"], ascending=False
+    )
+    return relevant_repos
+
+
+def get_stargazers(repo_fullname: str, github_token: str) -> List[NamedUser]:
+    try:
+        stargazers = Github(github_token).get_repo(repo_fullname).get_stargazers()
+        return stargazers
+    except:
+        print(f"[Error] failed to get stargazers of repo {repo_fullname}")
+        return []
+
+
+def aggregate_stargazers(repos: List[str], github_token: str) -> List[NamedUser]:
+    stargazers = {}
+    for repo in tqdm(repos):
+        repo_stargazers = get_stargazers(repo, github_token)
+        for stargazer in repo_stargazers:
+            if stargazer.login not in stargazers:
+                stargazers[stargazer.login] = stargazer
+    return list(stargazers.values())
+
+
+def diff_stargazers(
+    existed_stargazers: List[NamedUser], relevant_stargazers: List[NamedUser]
+) -> List[NamedUser]:
+    diffed_stargazers = []
+    existed_stargazers = {
+        stargazer.login: stargazer for stargazer in existed_stargazers
+    }
+    for stargazer in tqdm(relevant_stargazers):
+        if stargazer.login not in existed_stargazers:
+            diffed_stargazers.append(stargazer)
+    return diffed_stargazers
+
+
+def users_to_df(users: List[NamedUser]) -> pd.DataFrame:
+    data = []
+    for user in users:
+        data.append(
+            {
+                "login": user.login,
+                "name": user.name,
+                "email": user.email,
+                "twitter_username": user.twitter_username,
+                "bio": user.bio,
+                "website": user.blog,
+                "company": user.company,
+                "followers": user.followers,
+                "following": user.following,
+                "location": user.location,
+            }
+        )
+    df = pd.DataFrame(data)
+    return df
+
+
+def get_potential_stargazers(
+    owner: str, repo: str, query: str, db_path: Path, github_token: str
+) -> List[NamedUser]:
+    repo_stargazers = get_stargazers(f"{owner}/{repo}", github_token)
+
+    relevant_repos = list(find_relevant_repos(query, db_path)["full_name"])
+    relevant_stargazers = aggregate_stargazers(relevant_repos, github_token)
+
+    potential_stargazers = diff_stargazers(repo_stargazers, relevant_stargazers)
+    return potential_stargazers
+
+
+# @stub.function()
+# @web_endpoint(method="GET")
+# def web(owner: str, repo: str, query: str):
+#     answer = qanda_langchain(query)
+#     return {
+#         "answer": answer,
+#     }
+
+
+@stub.function()
+def cli(owner: str, repo: str, query: str):
+    # download db if not exist
+    db_path = Path(f"{repo}.lancedb")
+    # get potential stargazers
+    potential_stargazers = get_potential_stargazers(
+        owner, repo, query, db_path, os.getenv("GITHUB_ACCESS_TOKEN")
+    )
+    df = users_to_df(potential_stargazers)
+    print(df)
