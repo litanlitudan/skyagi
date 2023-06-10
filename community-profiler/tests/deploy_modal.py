a1cf3c6efc892a391a317dd3aaee5da61490b9d8
Tan Li <tan@tanli.dev>
2023-06-10 07:55:42 -0700

    update

--- community-profiler/tests/deploy_modal.py
+++ community-profiler/tests/deploy_modal.py
@@ -1,1 +1,17 @@
-url = "https://skyagi-public.s3.us-west-1.amazonaws.com/skyagi.zip"
+import requests
+import zipfile
+from pathlib import Path
+
+
+def download_db(repo="skyagi"):
+    lance_db = requests.get(
+        f"https://skyagi-public.s3.us-west-1.amazonaws.com/{repo}.zip"
+    )
+    with open(Path(f"{repo}.zip"), "wb") as f:
+        f.write(lance_db.content)
+
+    file = zipfile.ZipFile(Path(f"{repo}.zip"))
+    file.extractall(path=Path(f"{repo}_db"))
+
+
+download_db()
