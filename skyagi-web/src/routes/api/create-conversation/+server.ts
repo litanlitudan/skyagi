28db42b7c8d83e014a4be06c8c9263bc2644cc7e
Kejie Zhang <kejiezhang000@gmail.com>
2023-06-21 22:42:42 -0700

    fix: add compensation and commit transactions

--- skyagi-web/src/routes/api/create-conversation/+server.ts
+++ skyagi-web/src/routes/api/create-conversation/+server.ts
@@ -9,6 +9,11 @@ export const config: Config = {
     runtime: 'nodejs18.x'
 };
 
+enum Status {
+    SUCCESS = 'SUCCESS',
+    PENDING = 'PENDING'
+};
+
 export const PUT = (async ({ request, locals }: { request: Request; locals: App.Locals }) => {
     const {
         name,
@@ -18,7 +23,6 @@ export const PUT = (async ({ request, locals }: { request: Request; locals: App.
     } = await request.json();
 
     // get agents' initial memory and add to memory
-    const currentTime = new Date().toISOString();
     const initialMemoryEmbeddings: number[][] = [];
     const agentInfos: any[] = [];
 
@@ -26,12 +30,12 @@ export const PUT = (async ({ request, locals }: { request: Request; locals: App.
         const agent_id = agent.id;
         const embedding_model_settings = agent.embedding_model_settings;
 
-        const { data: agent_info } = await locals.supabase
+        const { data: agent_info, error } = await locals.supabase
             .from('agent')
             .select('initial_status, initial_memory')
             .eq('id', agent_id);
 
-        if (checkValidity(agent_info) === false) {
+        if (error || (checkValidity(agent_info) === false)) {
             return new Response(JSON.stringify({ 'success': 0, 'error': 'agent not found' }), { status: 200 });
         }
         agentInfos.push(agent_info);
@@ -50,24 +54,26 @@ export const PUT = (async ({ request, locals }: { request: Request; locals: App.
         initialMemoryEmbeddings.push(embedding);
     }
 
-    // Begin transaction
-
+    const currentTime = new Date().toISOString();
     // DB call: Create conversation
-    const { data: conv_id } = await locals.supabase
+    const { data: conv_id, error } = await locals.supabase
         .from('conversation')
         .insert({
             user_id: user_id,
             name: name,
             agents: agents.map((agent: { id: string; }) => agent.id),
-            user_agents: user_agent_ids
+            user_agents: user_agent_ids,
+            status: Status.PENDING,
+            createdAt: currentTime
         })
         .select('id');
 
-    if (checkValidity(conv_id) === false) {
-        return new Response(JSON.stringify({ 'success': 0, 'error': 'failed to find conversation' }), { status: 200 });
+    if (error || (checkValidity(conv_id) === false)) {
+        return new Response(JSON.stringify({ 'success': 0, 'error': 'failed to create conversation' }), { status: 200 });
     }
 
     // DB call: create Memory for all agents
+    const createdMemoryIds: string[] = [];
     agents.map(async (agent: { id: string; }, i: number) => {
         const agent_id = agent.id;
         const importance = 0.0;
@@ -80,21 +86,75 @@ export const PUT = (async ({ request, locals }: { request: Request; locals: App.
             last_access_time: currentTime
         };
 
-        const { error } = await locals.supabase
+        const { data: memory_id, error } = await locals.supabase
             .from('memory')
             .insert({
                 content: agentInfos[i][0].initial_memory,
                 embedding: initialMemoryEmbeddings[i],
-                metadata: metadata
-            });
+                metadata: metadata,
+                status: Status.PENDING,
+                createdAt: currentTime
+            })
+            .select('id');
 
-        if (error) {
+        if (error || (checkValidity(memory_id) === false)) {
+            // run compensation transactions
+            // 1. delete inserted conversation
+            const { error } = await locals.supabase
+                .from('conversation')
+                .delete()
+                .eq('id', conv_id[0].id);
+            if (error) {
+                // compensation transaction failed, we need to record and retry it again.
+                console.error(`Fail to run compensation transaction to delete conversation ${conv_id[0].id}, error: ${error.message}`);
+            }
+
+            // 2. delete inserted memories
+            for (let j = 0; j < i; ++j) {
+                const { error } = await locals.supabase
+                    .from('memory')
+                    .delete()
+                    .eq('id', createdMemoryIds[j]);
+                if (error) {
+                    // compensation transaction failed, we need to record and retry it again.
+                    console.error(`Fail to run compensation transaction to delete memory ${createdMemoryIds[j]}, error: ${error.message}`);
+                }
+            }
+
+            // error out
             return new Response(JSON.stringify({ 'success': 0, 'error': 'failed to create new initial memory' }), { status: 200 });
         }
+
+        createdMemoryIds.push(memory_id[0].id);
     });
 
-    // Rollback transaction
 
+    // commit the conversation and memory creation by updating the status to SUCCESS
+    const { error: updateConversationError } = await locals.supabase
+        .from('conversation')
+        .update({
+            status: Status.SUCCESS
+        })
+        .eq('id', conv_id[0].id);
+
+    if (updateConversationError) {
+        // commit transaction failed, we need to record and retry it again.
+        console.error(`Fail to update status to SUCCESS for conversation: ${conv_id[0].id}, error: ${updateConversationError.message}`);
+    }
+
+    for (const memory_id of createdMemoryIds) {
+        const { error } = await locals.supabase
+            .from('memory')
+            .update({
+                status: Status.SUCCESS
+            })
+            .eq('id', memory_id);
+
+        if (error) {
+            // commit transaction failed, we need to record and retry it again.
+            console.error(`Fail to update status to SUCCESS for memory: ${memory_id}, error: ${error.message}`);
+        }
+    }
 
     return new Response(JSON.stringify({ success: 1, conversation_id: conv_id[0].id }), { status: 200 });
 }) satisfies RequestHandler;
