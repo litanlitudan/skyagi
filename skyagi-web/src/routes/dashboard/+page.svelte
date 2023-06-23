91d3d4194f96a3fab2c53dcd339da5e1caf1a4e6
Qi Zheng <zhengqi1987@gmail.com>
2023-06-23 13:29:28 -0700

    change the style of the conversation board

--- skyagi-web/src/routes/dashboard/+page.svelte
+++ skyagi-web/src/routes/dashboard/+page.svelte
@@ -1,82 +1,79 @@
 <script lang="ts">
-    import Character from '$lib/dashboard-character.svelte';
-    import { AccordionItem, Accordion, Button} from 'flowbite-svelte';
-    import Conversation from '$lib/dashboard-conversation.svelte'
+	import Character from '$lib/dashboard-character.svelte';
+	import { AccordionItem, Accordion, Button } from 'flowbite-svelte';
+	import Conversation from '$lib/dashboard-conversation.svelte';
 	import { globalAvatarImageList } from '$lib/stores.js';
-    export let data;
-    const characterData = data.agents
-    const conversationData = data.conversations
-	export let activeId="";
-	if (conversationData.length != 0){
+	export let data;
+	const characterData = data.agents;
+	const conversationData = data.conversations;
+	export let activeId = '';
+	if (conversationData.length != 0) {
 		conversationData[0].conversationId;
 	}
-    
 
-    const characters = characterData.map(function(characterDataPoint) {
-		let imagePath = "/assets/Avatar1.png"
-		if (characterDataPoint.avatar!=null && globalAvatarImageList.includes(characterDataPoint.avatar.local_path)){
-			
-			imagePath = characterDataPoint.avatar.local_path
+	const characters = characterData.map(function (characterDataPoint) {
+		let imagePath = '/assets/Avatar1.png';
+		if (
+			characterDataPoint.avatar != null &&
+			globalAvatarImageList.includes(characterDataPoint.avatar.local_path)
+		) {
+			imagePath = characterDataPoint.avatar.local_path;
 		}
 		return {
 			...characterDataPoint,
 			image: imagePath
-    }})
+		};
+	});
 
-    
-    export const conversations = conversationData
-	let conversationOpenLs = conversations.map((item)=>false)
-	conversationOpenLs[0]=true
+	export const conversations = conversationData;
+	let conversationOpenLs = conversations.map(item => false);
+	conversationOpenLs[0] = true;
 
 	function handleResumeRoomClick() {
-		window.location.href = '/room/resume-room/' + activeId
+		window.location.href = '/room/resume-room/' + activeId;
 	}
 
-    function handleCreateAgentClick() {
-        window.location.href = '/agent/create'
-    }
+	function handleCreateAgentClick() {
+		window.location.href = '/agent/create';
+	}
 
-    function handleCreateRoomClick() {
-        window.location.href = '/room/new'
-    }
+	function handleCreateRoomClick() {
+		window.location.href = '/room/new';
+	}
 </script>
 
 <div id="globalGrid">
+	<div>
+		<Accordion
+			id="conversationBoard"
+			activeClasses="bg-gray-800 text-white focus:ring-4 focus:ring-blue-800"
+			inactiveClasses="text-gray-400 hover:bg-gray-800"
+		>
+			{#each conversations as conversation, i}
+				<Conversation
+					conversationIndex={i + 1}
+					conversationSummary={conversation}
+					conversationId={conversation.conversationId}
+					bind:activeId
+				/>
+			{/each}
+		</Accordion>
+		<div id="buttonGrid">
+			<Button on:click={handleResumeRoomClick}>Resume to the selected conversation</Button>
+			<Button on:click={handleCreateRoomClick}>Create new conversation</Button>
+			<Button on:click={handleCreateAgentClick}>Create new agent</Button>
+		</div>
+	</div>
 
-    <div>
-        <Accordion id="conversationBoard">
-            {#each conversations as conversation, i}
-                <Conversation conversationIndex={i+1} 
-				conversationSummary = {conversation} 
-				conversationId = {conversation.conversationId}
-				bind:activeId={activeId}>
-                </Conversation>
-				
-            {/each}
-        </Accordion>
-        <div id="buttonGrid">
-			<Button on:click={handleResumeRoomClick}>
-				Resume to the selected conversation
-			</Button>
-            <Button on:click={handleCreateRoomClick}>
-                Create new conversation
-            </Button>
-            <Button on:click={handleCreateAgentClick}>
-                Create new agent
-            </Button>
-        </div>
-    </div>
-    
-    <div class="scroller">
-        {#each characters as character, i}
-            <a href= "agent/{character.id}">
-            <div class="characterInfoSet">
-                <Character {character} imageUrl={character.image}>
-                </Character>
-            </div>
-            </a>
-        {/each}
-    </div>
+	<div class="scroller">
+		{#each characters as character, i}
+			<a href="agent/{character.id}">
+				<div class="characterInfoSet">
+					<Character {character} imageUrl={character.image} />
+				</div>
+			</a>
+		{/each}
+	</div>
 </div>
 
 <style>
@@ -95,10 +92,10 @@
 		grid-template-columns: repeat(3, 200px);
 		grid-template-rows: repeat(auto-fill, 220px);
 	}
-    #conversationBoard {
-        height: 1000px;
-        white-space: pre-line;
-    }
+	#conversationBoard {
+		height: 1000px;
+		white-space: pre-line;
+	}
 
 	#globalGrid {
 		display: grid;
