8a6161726cca86887555eca80172aca760e9643b
Qi Zheng <zhengqi1987@gmail.com>
2023-06-23 22:16:38 -0700

    change the scroller session

--- skyagi-web/src/lib/room-new-character.svelte
+++ skyagi-web/src/lib/room-new-character.svelte
@@ -1,46 +1,48 @@
 <script lang="ts">
 	import type { CharacterType } from './types';
-    import { createEventDispatcher } from 'svelte';
-    import { error } from '@sveltejs/kit';
-    import { Img } from 'flowbite-svelte';
-    import {notifications} from '$lib/notifications.js'
-	import Toast from '$lib/Toast.svelte'
-    import { browser } from '$app/environment';
+	import { createEventDispatcher } from 'svelte';
+	import { error } from '@sveltejs/kit';
+	import { Img } from 'flowbite-svelte';
+	import { notifications } from '$lib/notifications.js';
+	import Toast from '$lib/Toast.svelte';
+	import { browser } from '$app/environment';
 
 	export let character;
-    export let characters;
-    export let avatarStyle = "rounded-lg border-none border-4 hover:border-solid border-indigo-600";
+	export let characters;
+	export let avatarStyle = 'rounded-lg border-none border-4 hover:border-solid border-indigo-600';
 
-
-    // export let lastClickedCharacter;
-    const dispatch = createEventDispatcher();
-    function updateLastClickedCharacter(){
-        dispatch("message", {
-            character: character
-        })
-    };
-
-
-
+	// export let lastClickedCharacter;
+	const dispatch = createEventDispatcher();
+	function updateLastClickedCharacter() {
+		dispatch('message', {
+			character: character
+		});
+	}
 </script>
 
 <div class="container">
-    <figure>
-        <img src={character.image} alt="" on:click={updateLastClickedCharacter}
-        class={avatarStyle} width=100px />
-        <figcaption> {character.name} </figcaption>
-    </figure>
+	<figure>
+		<img
+			src={character.image}
+			alt=""
+			on:click={updateLastClickedCharacter}
+			class={avatarStyle}
+			width="100px"
+		/>
+		<figcaption class="text-lg mt-2">{character.name}</figcaption>
+	</figure>
 </div>
 <Toast />
+
 <style>
-    figure {
-        text-align: center;
-        width: 100px;
-    }
-    .container {
-        grid-template-columns: 20px 100px;
-        column-gap: 0px;
-        display: grid;
-        align-items: center;
-    }
+	figure {
+		text-align: center;
+		width: 100px;
+	}
+	.container {
+		grid-template-columns: 20px 100px;
+		column-gap: 0px;
+		display: grid;
+		align-items: center;
+	}
 </style>
