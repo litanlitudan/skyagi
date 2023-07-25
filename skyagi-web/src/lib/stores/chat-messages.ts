f598a7237066d7ab8f1bb7e21f6165996f798658
Serena Qiao <serenaqiao@Serenas-MacBook-Pro.local>
2023-07-24 21:27:29 -0700

    rebase to get stream ending

--- skyagi-web/src/lib/stores/chat-messages.ts
+++ skyagi-web/src/lib/stores/chat-messages.ts
@@ -13,13 +13,17 @@ const { subscribe, update, ...store } = writable<ChatTranscript>({
   chatState: 'idle'
 });
 
+const getModelInfoForAgent = (agentId: string) => {
+  let modelTokenData: { [key: string]: any } = JSON.parse(get(modelTokenDataStore));
+  return modelTokenData[agentId];
+};
+
 // put a new user input message to store
 const set = async (query: string) => {
   updateMessages(query, StoreMessageRole.USER_AGENT, 'Me', 'loading');
 
-  let modelTokenData: { [key: string]: any } = JSON.parse(get(modelTokenDataStore));
   const recipient_agent_id = get(currentAgentId);
-  const modelDataForCurrentAgent = modelTokenData[recipient_agent_id];
+  const modelDataForCurrentAgent = getModelInfoForAgent(recipient_agent_id);
 
   const request = {
     conversation_id: get(conversationId),
@@ -47,7 +51,7 @@ const set = async (query: string) => {
       }
     },
     message: query,
-  }
+  };
 
   const eventSource = new SSE('/api/send-conversation-message', {
     headers: {
@@ -76,9 +80,156 @@ const handleError = <T>(err: T) => {
   console.error(err);
 };
 
+const handleSystemMessage = (message: string) => {
+  updateMessages(message, StoreMessageRole.SYSTEM, 'bot', 'system');
+  console.info(message);
+};
+
+const getBiDirectionCombinations = (agentIds: string[]): string[][] => {
+  // eg, [a, b, c] -> [ab, ac, ba, bc, ca, cb]
+  if (!agentIds) return [];
+  let result = [];
+  for (var i = 0; i < agentIds.length; i++) {
+    // This is where you'll capture that last value
+    for (var j = 0; j < agentIds.length; j++) {
+      if (j !== i) result.push([agentIds[i], agentIds[j]]);
+    }
+  }
+  console.log('getBiDirectionCombinations', result);
+  return result;
+};
+
+// This will return message if content is valid
+// Otherwise, return empty content
+const getSendSystemMessageContentResult = async (conversationId: string, initiateAgentId: string, recipientAgentId: string) => {
+  const modelDataForCurrentAgent = getModelInfoForAgent(recipientAgentId);
+  const request = {
+    conversation_id: conversationId,
+    initiate_agent_id: initiateAgentId,
+    recipient_agent_id: recipientAgentId,
+    recipient_agent_model_settings: {
+      llm: {
+        type: modelDataForCurrentAgent.data.type,
+        provider: modelDataForCurrentAgent.data.provider,
+        name: modelDataForCurrentAgent.data.name,
+        args: {
+          modelName: modelDataForCurrentAgent.data.args.modelName,
+          maxTokens: modelDataForCurrentAgent.data.args.maxTokens,
+          openAIApiKey: modelDataForCurrentAgent.data.args.openAIApiKey,
+        }
+      },
+      embedding: {
+        type: modelDataForCurrentAgent.embedding.type,
+        provider: modelDataForCurrentAgent.embedding.provider,
+        name: modelDataForCurrentAgent.embedding.name,
+        args: {
+          modelName: modelDataForCurrentAgent.embedding.args.modelName,
+          openAIApiKey: modelDataForCurrentAgent.embedding.args.openAIApiKey,
+        }
+      }
+    },
+  };
+
+  const resp = await fetch('/api/send-system-message', {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'PUT',
+    body: JSON.stringify(request),
+  });
+
+  return resp.json();
+};
+
+const sendConversationMessage = async (conversationId: string, initiateAgentId: string, recipientAgentId: string, query: string) => {
+  const modelDataForRecipientAgent = getModelInfoForAgent(recipientAgentId);
+
+  const request = {
+    conversation_id: conversationId,
+    initiate_agent_id: initiateAgentId,
+    recipient_agent_id: recipientAgentId,
+    recipient_agent_model_settings: {
+      llm: {
+        type: modelDataForRecipientAgent.data.type,
+        provider: modelDataForRecipientAgent.data.provider,
+        name: modelDataForRecipientAgent.data.name,
+        args: {
+          modelName: modelDataForRecipientAgent.data.args.modelName,
+          maxTokens: modelDataForRecipientAgent.data.args.maxTokens,
+          openAIApiKey: modelDataForRecipientAgent.data.args.openAIApiKey,
+        }
+      },
+      embedding: {
+        type: modelDataForRecipientAgent.embedding.type,
+        provider: modelDataForRecipientAgent.embedding.provider,
+        name: modelDataForRecipientAgent.embedding.name,
+        args: {
+          modelName: modelDataForRecipientAgent.embedding.args.modelName,
+          openAIApiKey: modelDataForRecipientAgent.embedding.args.openAIApiKey,
+        }
+      }
+    },
+    message: query,
+  };
+
+  const resp = await fetch('/api/send-conversation-message', {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'PUT',
+    body: JSON.stringify(request),
+  });
+
+  return resp.json();
+};
+
+const getOneDirectionCombinations = (agentIds: string[]): string[][] => {
+  // eg, [a, b, c] -> [ab, ac, bc]
+  if (!agentIds) return [];
+  let result = [];
+  for (var i = 0; i < agentIds.length - 1; i++) {
+    // This is where you'll capture that last value
+    for (var j = i + 1; j < agentIds.length; j++) {
+      result.push([agentIds[i], agentIds[j]]);
+    }
+  }
+  console.log('getOneDirectionCombinations', result);
+  return result;
+};
+
+const agentsSystemMessagingProcess = async (biDirection = false) => {
+  // For each pair, start with calling system message. 
+  // eg, agent A asks agent B for system message, the result is the content that A want to talk to B
+  // After that we can use conversation message to let A talk to B with the content returned by system message endpoint
+  // the conversation keeps going until if_continue is false
+  // .... repeat above for other pairs.
+  const agentPairs: string[][] = biDirection ? getBiDirectionCombinations(get(agentIds)) : getOneDirectionCombinations(get(agentIds));
+  if (agentPairs) handleSystemMessage(`Something is happening around the world...`)
+  agentPairs.forEach(async pair => {
+    // Send system message to know the content to start conversation
+    const systemMessageResult = await getSendSystemMessageContentResult(get(conversationId), pair[0], pair[1]);
+    if (!systemMessageResult.success) {
+      handleError(`${pair[0]} ${pair[1]} failed to whisper`);
+    } else if (systemMessageResult.is_valid && systemMessageResult.message) {
+      handleSystemMessage(`${pair[0]} is whispering to ${pair[1]}`);
+      const initialConversationMessage = systemMessageResult.message;
+      // Send conversation messages between pair[0] and pair[1], until one of them doesn't want to continue
+      let shouldContinue = false; // TODO: change to True
+      let conversationContent = initialConversationMessage;
+      while (shouldContinue) {
+        const resp = sendConversationMessage(get(conversationId), pair[0], pair[1], conversationContent);
+        // Parse result
+      }
+    } else { //  System tells us that there's nothing to conversate between the two agents.
+
+    }
+  });
+};
+
 // put a new AI generated answer to store
 const streamMessage = (e: MessageEvent) => {
-
+  getBiDirectionCombinations(['a', 'b', 'c']);
+  getOneDirectionCombinations(['a', 'b', 'c']);
   try {
     if (e.data === '[DONE]') {
       updateMessages(get(answer), StoreMessageRole.AGENT, get(currentAgentName), 'idle');
@@ -96,11 +247,13 @@ const streamMessage = (e: MessageEvent) => {
       }
     } else { // not matching JSON regex meaning it's pure conversation content not JSON
       answer.update((_a) => _a + (e.data ? e.data : " "));
+      console.log('get(answer)', get(answer));
     }
   } catch (err) {
     handleError(err);
   }
 };
+
 
 // These variables are the memory store for current chat thread. 
 // aka, a specific combination of conversation id + agent id
